0e514e6139e849b7bfc4965863077f0530471d71
besrofex <bestdev29@gmail.com>
2025-02-09 15:24:53 +0100

    ||

--- minitalk-42/client.c
+++ minitalk-42/client.c
@@ -1,32 +1,29 @@
 #include "minitalk.h"
 
-void send_character(pid_t server_pid, char character) 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+void send_character(pid_t server_pid, char character)
 {
-	int	i;
-
-	i = 0;
-   while (i < 8) 
-	{
-        if (character & (1 << (7 - i)))
-            kill(server_pid, SIGUSR1);
-		  else 
-            kill(server_pid, SIGUSR2);
-        usleep(400);
-		  i++;
-   }
-}
-
-void send_message(pid_t server_pid, const char *message) {
-    while (*message) 
-	 {
-        send_character(server_pid, *message);
-        message++;
+    for (int i = 0; i < 8; i++) {
+        if (character & (1 << (7 - i))) {
+            kill(server_pid, SIGUSR1);  // Envoie SIGUSR1 pour bit 1
+        } else {
+            kill(server_pid, SIGUSR2);  // Envoie SIGUSR2 pour bit 0
+        }
+        usleep(100);  // Pause entre les signaux, ajuster la vitesse si nécessaire
     }
 }
 
-void confirmation_handler(int sig) {
-    if (sig == SIGUSR1)
-        printf("Le serveur a confirmé la réception du message.\n");
+void send_message(pid_t server_pid, const char *message)
+{
+    while (*message) {
+        send_character(server_pid, *message);  // Envoi de chaque caractère
+        message++;
+    }
+    send_character(server_pid, '\0');  // Envoi d'un caractère NULL pour signaler la fin du message
 }
 
 int main(int argc, char *argv[])
@@ -35,18 +32,16 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Usage: %s <PID du serveur> <message>\n", argv[0]);
         exit(1);
     }
+
     pid_t server_pid = atoi(argv[1]);
     const char *message = argv[2];
-    struct sigaction sa;
-    sa.sa_flags = 0;
-    sa.sa_handler = confirmation_handler;
-    if (sigaction(SIGUSR1, &sa, NULL) == -1) {
-        perror("Erreur avec sigaction pour SIGUSR1");
-        exit(1);
-    }
+
+    // Envoi du message au serveur
     send_message(server_pid, message);
-    printf("Message envoyé : %s\n", message);
-    pause();
+   //  printf("Message envoyé : %s\n", message);
+
     return 0;
 }
+
+
 
--- minitalk-42/server.c
+++ minitalk-42/server.c
@@ -1,49 +1,64 @@
 #include "minitalk.h"
 
-volatile sig_atomic_t current_char = 0;  // Variable pour suivre la position du caractère actuel
-char received_string[1024]; // Tableau pour stocker la chaîne reçue
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
 
-void handler(int sig, siginfo_t *info, void *ucontext) {
-    static int bit_position = 0;
-    static unsigned char character = 0;
+static char current_char = 0;
+static int bit_count = 0;
+static int message_received = 0;
 
+void signal_handler(int sig)
+{
     if (sig == SIGUSR1)
-        character |= (1 << (7 - bit_position));
-    bit_position++;
-    if (bit_position == 8) 
-	 {
-        received_string[current_char++] = character;
-        character = 0;
-        bit_position = 0;
-        if (current_char >= sizeof(received_string) - 1) 
-		  {
-            received_string[current_char] = '\0';
-            printf("Message reçu : %s\n", received_string);
-            current_char = 0;
-            // kill(info->si_pid, SIGUSR1);
+        current_char |= (1 << (7 - bit_count)); // Si SIGUSR1, on met à 1 le bit correspondant.
+    // SIGUSR2 ne fait rien, donc on n'a pas besoin d'assigner un bit.
+
+    bit_count++;
+
+    // Si on a reçu 8 bits, on a un caractère complet.
+    if (bit_count == 8) {
+        if (current_char == '\0') {  // Si c'est un caractère NULL, on termine le message
+            message_received = 1;
+            printf("\n");
+        } else {
+            printf("%c", current_char);  // Affichage du caractère reçu
         }
+        current_char = 0;  // Réinitialiser pour le prochain caractère
+        bit_count = 0;     // Réinitialiser le compteur de bits
     }
 }
 
-int main() 
+int main()
 {
     struct sigaction sa;
 
-    sa.sa_flags = SA_SIGINFO;
-    sa.sa_sigaction = handler;
-    if (sigaction(SIGUSR1, &sa, NULL) == -1) {
-        perror("Erreur avec sigaction SIGUSR1");
+    // Affichage du PID du serveur
+    printf("Serveur lancé avec le PID : %d\n", getpid());
+
+    // Configuration du gestionnaire de signaux pour SIGUSR1 et SIGUSR2
+    sa.sa_flags = 0;
+    sa.sa_handler = signal_handler;
+    if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1) {
+        perror("Erreur avec sigaction");
         exit(1);
     }
-    if (sigaction(SIGUSR2, &sa, NULL) == -1) {
-        perror("Erreur avec sigaction SIGUSR2");
-        exit(1);
-    }
-    printf("PID : %d\n", getpid());
+
+    // Le serveur peut recevoir plusieurs messages
     while (1) {
-        pause();
+        // Attente que tous les signaux pour un message aient été reçus
+        while (!message_received) {
+            pause();  // Bloque l'exécution et attend les signaux
+        }
+
+        // Réinitialisation pour le prochain message
+        message_received = 0;
     }
+
     return 0;
 }
+
+
 
 
